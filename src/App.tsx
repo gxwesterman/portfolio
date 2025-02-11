e9979890bf3925f777a9ec2de6f3e6617d1d3b39
root <root@localhost.localdomain>
2025-02-11 12:21:25 -0600

    feat: added theme functionality and toggle, updated favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
-import { Button } from "@/components/ui/button"
+import { ThemeProvider } from "@/components/theme-provider"
+import { ModeToggle} from '@/components/mode-toggle';
 
 function App() {
 
-  return <Button>Button</Button>
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="w-screen h-screen">
+        <div className="flex items-center justify-end p-4 gap-2">
+          <ModeToggle />
+        </div>
+      </div>
+    </ThemeProvider>
+  )
 }
 
 export default App
