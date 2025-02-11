9223b145da251f84b3700289acf09a3e38d54e40
root <root@localhost.localdomain>
2025-02-11 16:59:56 -0600

    feat: working on carousel

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { ThemeProvider } from "@/components/theme-provider"
 import { ModeToggle} from '@/components/mode-toggle'
 import { GithubButton} from '@/components/github-button'
+import { Carousel } from '@/components/carousel'
 import { ScrollArea } from "@/components/ui/scroll-area"
 
 function App() {
@@ -13,26 +14,31 @@ function App() {
           <div className="flex justify-start items-end flex-col mt-20 pl-[20%]">
             <div className="text-[40px] font-bold">Griffin Westerman</div>
             <div>Software Developer</div>
-            <div className="flex items-center justify-end gap-2 mt-5">
-              <GithubButton />
-              <ModeToggle />
+            <div className="space-y-5 mt-5">
+              <div className="flex items-center justify-end gap-5">
+                <GithubButton />
+                <ModeToggle />
+              </div>
+              <Carousel />
             </div>
           </div>
           <ScrollArea className="w-full max-h-screen py-20 pr-[20%]">
-            <div className="w-full h-50 rounded-md mb-10 p-5">
-              <p>
-                Howdy y'all. I graduated from the U of M in 2023, and have worked as a Software Developer at Nagios since.
-                I contributed to and even lead major releases of enterprise products, helping to move software more than a decade forward.
-              </p>
-              <br/>
-              <p>I'm dedicated to learning as much as I can wherever I go.</p>
-              <br/>
-              <p>I'm also a classically trained cellist and love the arts and the outdoors. Hmu if you wanna play tennis sometime.</p>
+            <div>
+              <div className="w-full h-50 rounded-md mb-10 p-5">
+                <p>
+                  Howdy y'all. I graduated from the U of M in 2023, and have worked as a Software Developer at Nagios since.
+                  I contributed to and even lead major releases of enterprise products, helping to move software more than a decade forward.
+                </p>
+                <br/>
+                <p>I'm dedicated to learning as much as I can wherever I go.</p>
+                <br/>
+                <p>I'm also a classically trained cellist and love the arts and the outdoors. Hmu if you wanna play tennis sometime.</p>
+              </div>
+              <div className="w-full h-50 rounded-md mb-5 p-5 hover:bg-orange-400/30">test</div>
+              <div className="w-full h-50 rounded-md mb-5 p-5 hover:bg-orange-400/30">test</div>
+              <div className="w-full h-50 rounded-md mb-5 p-5 hover:bg-orange-400/30">test</div>
+              <div className="w-full h-50 rounded-md p-5 hover:bg-orange-400/30">test</div>
             </div>
-            <div className="w-full h-50 border border-border rounded-md mb-5"></div>
-            <div className="w-full h-50 border border-border rounded-md mb-5"></div>
-            <div className="w-full h-50 border border-border rounded-md mb-5"></div>
-            <div className="w-full h-50 border border-border rounded-md"></div>
           </ScrollArea>
         </div>
       </div>
