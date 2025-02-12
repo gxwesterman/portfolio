378ab0740e44eaf41e220ad4847d4a81acfd39f0
root <root@localhost.localdomain>
2025-02-12 13:12:36 -0600

    feat: got carousel working, updated info

--- src/App.tsx
+++ src/App.tsx
@@ -11,20 +11,20 @@ function App() {
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <div className="w-auto h-auto">
         <div className="grid grid-cols-2 gap-20">
-          <div className="flex justify-start items-end flex-col mt-20 pl-[20%]">
+          <div className="flex justify-start items-start flex-col mt-20 pl-[50%]">
             <div className="text-[40px] font-bold">Griffin Westerman</div>
-            <div>Software Developer</div>
+            <div className="text-lg">Software Developer</div>
             <div className="space-y-5 mt-5">
-              <div className="flex items-center justify-end gap-5">
+              <div className="flex items-center justify-start gap-5">
                 <GithubButton />
                 <ModeToggle />
               </div>
               <Carousel />
             </div>
           </div>
-          <ScrollArea className="w-full max-h-screen py-20 pr-[20%]">
-            <div>
-              <div className="w-full h-50 rounded-md mb-10 p-5">
+          <ScrollArea className="w-full max-h-screen py-20 pr-[30%]">
+            <div className="space-y-10">
+              <div className="w-fit h-fit rounded-md p-5">
                 <p>
                   Howdy y'all. I graduated from the U of M in 2023, and have worked as a Software Developer at Nagios since.
                   I contributed to and even lead major releases of enterprise products, helping to move software more than a decade forward.
@@ -34,10 +34,17 @@ function App() {
                 <br/>
                 <p>I'm also a classically trained cellist and love the arts and the outdoors. Hmu if you wanna play tennis sometime.</p>
               </div>
-              <div className="w-full h-50 rounded-md mb-5 p-5 hover:bg-orange-400/30">test</div>
-              <div className="w-full h-50 rounded-md mb-5 p-5 hover:bg-orange-400/30">test</div>
-              <div className="w-full h-50 rounded-md mb-5 p-5 hover:bg-orange-400/30">test</div>
-              <div className="w-full h-50 rounded-md p-5 hover:bg-orange-400/30">test</div>
+              <div className="w-full h-fit rounded-md p-5 hover:bg-secondary flex gap-5">
+                <div className="text-primary/60">August 2023 - Present</div>
+                <div className="space-y-2">
+                  <div>Full Stack Software Developer - Nagios</div>
+                  <div className="ml-3 text-foreground/80">
+                    <li>Led the frontend overhaul of Nagios Log Server, rewriting a PHP codebase to a React app leveraging tailwind, shadcn, react router, and much more</li>
+                    <li>Co-led the complete overhaul of Nagios Log Server, converting an Elasticsearch backend to Opensearch, and modernizing the UI and UX</li>
+                    <li>Developed numerous features and resolved even more bugs for the Nagios suite of products, consistently working with both the frontend and backend</li>
+                  </div>
+                </div>
+              </div>
             </div>
           </ScrollArea>
         </div>
